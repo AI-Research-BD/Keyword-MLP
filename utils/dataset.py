1cc3113dedb0cba15fe420611e42c34ce33cbd49
ID56 <mashrurmahmud@iut-dhaka.edu>
2021-10-15 21:37:51 +0600

    Added inference scripts.

--- utils/dataset.py
+++ utils/dataset.py
@@ -16,7 +16,6 @@ from audiomentations import AddBackgroundNoise
 
 def get_train_val_test_split(root: str, val_file: str, test_file: str):
     """Creates train, val, and test split according to provided val and test files.
-
     Args:
         root (str): Path to base directory of the dataset.
         val_file (str): Path to file containing list of validation data files.
@@ -67,28 +66,28 @@ def get_train_val_test_split(root: str, val_file: str, test_file: str):
 class GoogleSpeechDataset(Dataset):
     """Dataset wrapper for Google Speech Commands V2."""
     
-    def __init__(self, data_list: list, label_map: dict, audio_settings: dict, aug_settings: dict = None, cache: int = 0):
+    def __init__(self, data_list: list, audio_settings: dict, label_map: dict = None, aug_settings: dict = None, cache: int = 0):
         super().__init__()
 
-        self.label_2_idx = {v: int(k) for k, v in label_map.items()}
         self.audio_settings = audio_settings
         self.aug_settings = aug_settings
         self.cache = cache
-        
 
         if cache:
             print("Caching dataset into memory.")
-            self.data_list = init_cache(data_list, audio_settings["sr"], cache, audio_settings, aug_settings)
-
+            self.data_list = init_cache(data_list, audio_settings["sr"], cache, audio_settings)
         else:
             self.data_list = data_list
             
-        # labels
-        self.label_list = []
-        for path in data_list:
-            self.label_list.append(self.label_2_idx[path.split("/")[-2]])
+        # labels: if no label map is provided, will not load labels. (Use for inference)
+        if label_map is not None:
+            self.label_list = []
+            label_2_idx = {v: int(k) for k, v in label_map.items()}
+            for path in data_list:
+                self.label_list.append(label_2_idx[path.split("/")[-2]])
+        else:
+            self.label_list = None
         
-        self.bg_adder = None
 
         if aug_settings is not None:
             if "bg_noise" in self.aug_settings:
@@ -105,33 +104,40 @@ class GoogleSpeechDataset(Dataset):
         else:
             x = librosa.load(self.data_list[idx], self.audio_settings["sr"])[0]
 
-        x = self.transform(x, self.cache, self.audio_settings, self.aug_settings, self.bg_adder)
-        x = torch.from_numpy(x).float().unsqueeze(0)
+        x = self.transform(x)
+
+        if self.label_list is not None:
+            label = self.label_list[idx]
+            return x, label
+        else:
+            return x
+
+
+    def transform(self, x):
+        """Applies necessary preprocessing to audio.
+        Args:
+            x (np.ndarray) - Input waveform; array of shape (n_samples, ).
         
-        label = self.label_list[idx]
+        Returns:
+            x (torch.FloatTensor) - MFCC matrix of shape (n_mfcc, T).
+        """
 
-        return x, label
-
-
-    @staticmethod
-    def transform(x: np.ndarray, cache_level: int, audio_settings: dict, aug_settings: dict = None, bg_adder = None, initmode: bool = False):
-
-        sr = audio_settings["sr"]
+        sr = self.audio_settings["sr"]
 
         ###################
         # Waveform 
         ###################
 
-        if cache_level < 2 or initmode:
-            if aug_settings is not None:
-                if "bg_noise" in aug_settings:
-                    x = bg_adder(samples=x, sample_rate=sr)
+        if self.cache < 2:
+            if self.aug_settings is not None:
+                if "bg_noise" in self.aug_settings:
+                    x = self.bg_adder(samples=x, sample_rate=sr)
 
-                if "time_shift" in aug_settings:
-                    x = time_shift(x, sr, **aug_settings["time_shift"])
+                if "time_shift" in self.aug_settings:
+                    x = time_shift(x, sr, **self.aug_settings["time_shift"])
 
-                if "resample" in aug_settings:
-                    x, _ = resample(x, sr, **aug_settings["resample"])
+                if "resample" in self.aug_settings:
+                    x, _ = resample(x, sr, **self.aug_settings["resample"])
             
             x = librosa.util.fix_length(x, sr)
 
@@ -139,52 +145,40 @@ class GoogleSpeechDataset(Dataset):
             # Spectrogram
             ###################
         
-            x = librosa.feature.melspectrogram(y=x, **audio_settings)        
-            x = librosa.feature.mfcc(S=librosa.power_to_db(x), n_mfcc=audio_settings["n_mels"])
+            x = librosa.feature.melspectrogram(y=x, **self.audio_settings)        
+            x = librosa.feature.mfcc(S=librosa.power_to_db(x), n_mfcc=self.audio_settings["n_mels"])
 
-        if not initmode and aug_settings is not None:
-            if "spec_aug" in aug_settings:
-                x = spec_augment(x, **aug_settings["spec_aug"])
 
+        if self.aug_settings is not None:
+            if "spec_aug" in self.aug_settings:
+                x = spec_augment(x, **self.aug_settings["spec_aug"])
+
+        x = torch.from_numpy(x).float().unsqueeze(0)
         return x
 
 
-
-def cache_item_loader(path: str, sr: int, cache_level: int, audio_settings: dict, aug_settings: dict, bg_adder=None) -> np.ndarray:
+def cache_item_loader(path: str, sr: int, cache_level: int, audio_settings: dict) -> np.ndarray:
     x = librosa.load(path, sr)[0]
     if cache_level == 2:
-        x = GoogleSpeechDataset.transform(
-            x,
-            cache_level,
-            audio_settings,
-            aug_settings,
-            bg_adder,
-            initmode=True
-        )
+        x = librosa.util.fix_length(x, sr)
+        x = librosa.feature.melspectrogram(y=x, **audio_settings)        
+        x = librosa.feature.mfcc(S=librosa.power_to_db(x), n_mfcc=audio_settings["n_mels"])
     return x
 
 
-def init_cache(data_list: list, sr: int, cache_level: int, audio_settings: dict, aug_settings: dict, n_cache_workers: int = 4) -> np.ndarray:
+def init_cache(data_list: list, sr: int, cache_level: int, audio_settings: dict, n_cache_workers: int = 4) -> list:
     """Loads entire dataset into memory for later use.
-
     Args:
         data_list (list): List of data items.
         sr (int): Sampling rate.
         cache_level (int): Cache levels, one of (1, 2), caching wavs and spectrograms respectively.
         n_cache_workers (int, optional): Number of workers. Defaults to 4.
-
     Returns:
-        cache (np.ndarray): Array containing data.
+        cache (list): List of data items.
     """
 
     cache = []
-    
-    bg_adder=None
-    if aug_settings is not None:
-        if "bg_noise" in aug_settings:
-            bg_adder = AddBackgroundNoise(sounds_path=aug_settings["bg_noise"]["bg_folder"])
-
-    loader_fn = functools.partial(cache_item_loader, sr=sr, cache_level=cache_level, audio_settings=audio_settings, aug_settings=aug_settings, bg_adder=bg_adder)
+    loader_fn = functools.partial(cache_item_loader, sr=sr, cache_level=cache_level, audio_settings=audio_settings)
 
     pool = mp.Pool(n_cache_workers)
 
@@ -194,13 +188,11 @@ def init_cache(data_list: list, sr: int, cache_level: int, audio_settings: dict,
     pool.close()
     pool.join()
 
-    cache = np.stack(cache)
     return cache
 
 
 def get_loader(data_list, config, train=True):
     """Creates dataloaders for training, validation and testing.
-
     Args:
         config (dict): Dict containing various settings for the training run.
         train (bool): Training or evaluation mode.
@@ -229,5 +221,3 @@ def get_loader(data_list, config, train=True):
     )
 
     return dataloader
-
-    
