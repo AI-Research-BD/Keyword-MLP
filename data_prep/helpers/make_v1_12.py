3109c66d44b0cdd85547c7edbd8adfbabfd601c0
ID56 <mashrurmahmud@iut-dhaka.edu>
2022-03-06 18:00:27 +0600

    Stratify unknown and silence classes in a deterministic way.

--- data_prep/helpers/make_v1_12.py
+++ data_prep/helpers/make_v1_12.py
@@ -1,6 +1,9 @@
 import json
 from argparse import ArgumentParser
+
+from sklearn.utils import shuffle
 from utils.dataset import get_train_val_test_split
+from sklearn.model_selection import train_test_split
 import os
 import glob
 import numpy as np
@@ -14,18 +17,25 @@ def delete_files_from_list(file_list):
 
 
 def make_unknown(data_list, unknown_perc, silence_perc, label_map, root):
-    known_list, unknown_list = [], []
+    known_list, unknown_list, unknown_class_list = [], [], []
     for file_path in data_list:
-        if file_path.split("/")[-2] in label_map.values():
+        classname = file_path.split("/")[-2]
+        if classname in label_map.values():
             known_list.append(file_path)
         else:
             unknown_list.append(file_path)
+            unknown_class_list.append(classname)
     
     known_perc = 1 - (unknown_perc + silence_perc)
-    num_total_data = int(len(known_list) / known_perc)
+    num_total_data = int(len(known_list) / known_perc) # Expected size of whole dataset
     num_unknown = int(num_total_data * unknown_perc)
+    unknown_frac = num_unknown / len(unknown_list) # fraction of unknown list which will go into data_list
 
-    unknown_list = np.random.choice(unknown_list, num_unknown, replace=False).tolist()
+    # use train_test_split to sample unknowns with stratification
+    unknown_list, _, _, _ = train_test_split(unknown_list, unknown_class_list, train_size=unknown_frac,
+                            random_state=0, shuffle=True, stratify=unknown_class_list)
+
+    # unknown_list = np.random.choice(unknown_list, num_unknown, replace=False).tolist()
     renamed_unknown_list = []
 
     for file_path in unknown_list:
@@ -37,28 +47,48 @@ def make_unknown(data_list, unknown_perc, silence_perc, label_map, root):
     return known_list + renamed_unknown_list, num_total_data
 
 
-def create_silence(root, stride = 2000):
+def create_silence(root, train_list, val_list, total_train_size, total_val_size):
+    stride = 2000
+    all_silence, silence_source = [], []
+    
     for bg_file in glob.glob(os.path.join(root, "_background_noise_", "*.wav")):
         y, sr = sf.read(bg_file)
         for i in range(0, len(y) - sr, stride):
             out_path = os.path.join(root, "_silence_", f"{os.path.basename(bg_file)[:-4]}_{i}.wav")
             sf.write(out_path, y[i: i + sr], sr)
-
-
-def add_silence(train_list, val_list, total_train_size, total_val_size, root):
+            all_silence.append(out_path)
+            silence_source.append(bg_file)
+    
     num_silence_train = total_train_size - len(train_list)
     num_silence_val = total_val_size - len(val_list)
 
-    all_silence = glob.glob(os.path.join(root, "_silence_", "*.wav"))
-    train_val = np.random.choice(all_silence, num_silence_train + num_silence_val, replace=False).tolist()
+    num_silence_train_val = num_silence_train + num_silence_val
+    silence_frac = num_silence_train_val / len(all_silence)
+
+    train_val_silence, to_be_removed_silence, train_val_silence_source, _ = train_test_split(all_silence, silence_source, train_size=silence_frac,
+                            random_state=0, shuffle=True, stratify=silence_source)
+    silence_train_frac = num_silence_train / len(train_val_silence)
+    train_silence, val_silence, _, _ = train_test_split(train_val_silence, train_val_silence_source, train_size=silence_train_frac,
+                            random_state=0, shuffle=True, stratify=train_val_silence_source)
+
+    delete_files_from_list(to_be_removed_silence)
+    return train_list + train_silence, val_list + val_silence
+
+
+# def add_silence(train_list, val_list, total_train_size, total_val_size, root):
+#     num_silence_train = total_train_size - len(train_list)
+#     num_silence_val = total_val_size - len(val_list)
+
+#     all_silence = glob.glob(os.path.join(root, "_silence_", "*.wav"))
+#     train_val = np.random.choice(all_silence, num_silence_train + num_silence_val, replace=False).tolist()
     
-    to_be_removed = list(set(all_silence) - set(train_val))
-    delete_files_from_list(to_be_removed)
+#     to_be_removed = list(set(all_silence) - set(train_val))
+#     delete_files_from_list(to_be_removed)
 
-    train_list = train_list + train_val[:num_silence_train]
-    val_list = val_list + train_val[num_silence_train:]
+#     train_list = train_list + train_val[:num_silence_train]
+#     val_list = val_list + train_val[num_silence_train:]
 
-    return train_list, val_list
+#     return train_list, val_list
 
 
 
@@ -81,13 +111,11 @@ def main(args):
     # print(total_train_size, len(train_list))
     # print(total_val_size, len(val_list))
 
-    create_silence(args.root)
-    print(f"Created {len(os.listdir(os.path.join(args.root, '_silence_')))} silence files.")
-
-    train_list, val_list = add_silence(train_list, val_list, total_train_size, total_val_size, args.root)
-    print("Added silence to train and val.")
+    train_list, val_list = create_silence(args.root, train_list, val_list, total_train_size, total_val_size)
+    print(f"Created silence files for train and val.")
 
     delete_files_from_list(test_list)
+    
     print("Deleted test_list files.")
 
     for classdir in glob.glob(os.path.join(args.root, "*")):
