ef070934aebe118f03682db49da40c1d8a53ae06
Mashrur Morshed <38450043+ID56@users.noreply.github.com>
2022-03-27 20:09:12 +0600

    Knowledge Distillation (#5)
    * Updated gitignore
    
    * Added distillation loss
    
    * Added freeze utility
    
    * Added distillation settings in base config.
    
    * Added distillation utils and distillation training code.
    
    * Fixed small bugs in distill utils
    
    * Temperating scaling to T_min
    
    * Pass root to dataset

--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 
 class LabelSmoothingLoss(nn.Module):
@@ -11,7 +12,7 @@ class LabelSmoothingLoss(nn.Module):
         dim (int, optional): Dimension across which to apply loss. Defaults to -1.
     """
 
-    def __init__(self, num_classes: int, smoothing : float = 0.1, dim : int = -1):
+    def __init__(self, num_classes: int, smoothing: float = 0.1, dim: int = -1):
         """Initializer for LabelSmoothingLoss.
 
         Args:
@@ -45,3 +46,44 @@ class LabelSmoothingLoss(nn.Module):
             true_dist.fill_(self.smoothing / (self.cls - 1))
             true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
         return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
+
+
+class KDLoss(nn.Module):
+    """Knowledge Distillation loss."""
+
+    def __init__(self, num_classes: int, dim: int = -1):
+        """Initializer for KDLoss.
+
+        Args:
+            num_classes (int): Number of target classes.
+            dim (int, optional): Dimension across which to apply loss. Defaults to -1.
+        """
+        super().__init__()
+        
+        self.cls = num_classes
+        self.dim = dim
+
+    def forward(self, pred: torch.Tensor, target: torch.Tensor, teacher_pred: torch.Tensor, alpha: float, T: float) -> torch.Tensor:
+        """Forward method for KDLoss.
+
+        Args:
+            pred (torch.Tensor): Predictions of student model. Tensor of shape (batch, num_classes).
+            target (torch.Tensor): Labels. LongTensor of shape (batch,), containing class integers like [1, 2, 3, ...].
+            teacher_pred (torch.Tensor): Predictions of teacher model. Tensor of shape (batch, num_classes).
+            alpha (float): Weight parameter.
+            T (float): Temperature for KDLoss.
+
+        Returns:
+            torch.Tensor: Loss value.
+        """
+
+        pred_log_probs = F.log_softmax(pred / T, dim=self.dim)
+        teacher_pred_log_probs = F.log_softmax(teacher_pred / T, dim=self.dim)
+
+        kldiv = F.kl_div(pred_log_probs, teacher_pred_log_probs, log_target=True)
+        
+        crossentropy = F.cross_entropy(pred, target)
+
+        return (alpha * T * T) * kldiv + (1.0 - alpha) * crossentropy
+
+        
